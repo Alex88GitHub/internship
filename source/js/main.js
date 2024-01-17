02fcd3ea7a28b48736bbf7d94d276ea61166446e
unknown <a-golovko2009@yandex,ru>
2024-01-17 16:58:24 +0500

    перед аккордионом и переделкой кнопки меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
+import Swiper from '../../node_modules/swiper';
+import {Navigation, Pagination} from '../../node_modules/swiper/modules';
 // import 'swiper/css';
 
