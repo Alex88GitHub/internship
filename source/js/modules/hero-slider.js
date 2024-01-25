e02f5e7d8c2bbd24841de57d99ce03ee188c4613
unknown <a-golovko2009@yandex,ru>
2024-01-25 13:42:33 +0500

    переход на ПК

--- source/js/modules/hero-slider.js
+++ source/js/modules/hero-slider.js
@@ -1,19 +1,30 @@
 import Swiper from 'swiper/bundle';
 // import {Pagination} from 'swiper/modules';
 
+// const slider = document.querySelector('[data-slider="hero-slider"]');
+// // const pagination = slider.querySelector('[data-slider="hero-pagination"]');
+
 export const heroSlider = () => {
   const slider = document.querySelector('[data-slider="hero-slider"]');
-  const pagination = slider.querySelector('[data-slider="hero-pagination"]');
+  // const pagination = slider.querySelector('[data-slider="hero-pagination"]');
 
-  return new Swiper(slider, {
+  const sli = new Swiper(slider, {
+    observer: true,
     loop: true,
     // autoHeight: true,
     // autoplay: {
     //   delay: 3000,
     // },
     pagination: {
-      el: pagination,
+      el: '.swiper-slide-active [data-slider="hero-pagination"]',
       clickable: true,
+    },
+    on: {
+      slideChangeTransitionStart: function () {
+        sli.pagination.init();
+        sli.pagination.render();
+        sli.pagination.update();
+      }
     },
 
     breakpoints: {
